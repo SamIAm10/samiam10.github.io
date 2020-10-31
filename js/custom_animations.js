1019c0529e669ef4bbddf46f217cd9463ff12377
Wissam <wissamhisham@gmail.com>
2020-10-30 22:31:14 -0700

    fixed animations

--- js/custom_animations.js
+++ js/custom_animations.js
@@ -1,5 +1,4 @@
 function bounceUp() {
-    document.getElementsByClassName("email-icon")[0].style.transition = "all .2s ease-in-out";
     document.getElementsByClassName("email-icon")[0].style.transform = "translate(30%, -30%)";
 }
 
