a11f201beb41875f0f99764c4e7f5185611f331f
Wissam <wissamhisham@gmail.com>
2020-11-09 21:42:53 -0800

    update dark mode

---
+++ js/dark-mode.js
@@ -0,0 +1,28 @@
+// var dark_enabled = false;
+
+// function darkMode() {
+//     document.getElementById("about").style.background = "black";
+//     document.getElementById("about").style.color = "white";
+//     document.getElementById("tl-dr").style.background = "black"
+//     document.getElementById("tl-dr").style.color = "white";
+//     document.querySelector("#tl-dr h3").style.color = "white";
+//     dark_enabled = true;
+// }
+
+// function lightMode() {
+//     document.getElementById("about").style.background = "white";
+//     document.getElementById("about").style.color = "black";
+//     document.getElementById("tl-dr").style.background = "#f0f0f0"
+//     document.getElementById("tl-dr").style.color = "#555";
+//     document.querySelector("#tl-dr h3").style.color = "black";
+//     dark_enabled = false;
+// }
+
+// function toggle() {
+//     dark_enabled ? lightMode() : darkMode();
+// }
+
+// function toggle() {
+//     document.getElementById("about").classList.toggle("dark-about");
+//     document.getElementById("tl-dr").classList.toggle("dark-tl-dr");
+// }
